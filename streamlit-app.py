999337ea5bf73c9765311ee21d1d09ecf2f9c993
sai-kumar-aallaa <114865294+sai-kumar-aallaa@users.noreply.github.com>
2022-10-04 01:04:48 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -21,6 +21,18 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 
+#new section to display fruityvice api response 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+
+
+
+
+
+
+
 
 
 
